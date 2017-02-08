b28f7216b9cc8fd17fcda9a2412df795a9933c96
Kaitlyn Deppmeier <kdeppmeier@yahoo.com>
2017-02-07 17:31:56 -0800

    Created EnemyTriggerBox
    This is a basic trigger script that can be attached to a collider.  Will make enemies appear when the player walks into the collider.

---
+++ Assets/_Scripts/EnemyTriggerBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTriggerBox : MonoBehaviour
+{
+    //This is a basic enemy trigger.  More complex ones will require some more coding.
+
+    //List of enemies that will appear when the player walks into the triggerbox
+    //Enemies must be set to inactive initially.
+    public GameObject[] enemies;
+
+	void OnTriggerEnter(Collider other)
+    {
+        //Checks if the player entered.  Activates the enemies if the player did enter.
+        if(other.tag.Equals("Player"))
+        {
+            for(int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].SetActive(true);
+            }
+            //Deactivates the collider.
+            gameObject.SetActive(false);
+        }
+    }
+}
