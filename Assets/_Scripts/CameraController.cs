efff2484e22bcb6e907290ffc34967e9d94dfd1a
Kawdek <rexg@uci.edu>
2017-02-26 15:38:49 -0800

    Camera, Player Animation Timing, Player Damage
    I've modified the camera such that the camera only moves if the mouse is
    outside the center area of the screen. I've also adjusted player
    animation timing somewhat for fluidity. I've shamelessly (but not
    without gratitude) integrated Kaitlyn's damage system into the player
    controller with the slight change of making the player animations an
    array due to multiple attack animations for the player.

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    
+    private float rotationSpeed = 150f;
+    private Vector3 point;
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        //Ensures that the camera is not too low (for the timebeing)
+        if (transform.position.y < player.transform.position.y + 2)
+            transform.position += Vector3.up * Time.deltaTime;
+
+        //Horizontal displacement from center
+        float mouseH = Input.mousePosition.x - Screen.width / 2;
+        if (mouseH < (float)Screen.width / 6 && mouseH > - (float)Screen.width / 6)
+            mouseH = 0;
+
+        //Rotates the camera around the player according to position of mouse on screen
+        //(Can be changed to keyboard controls if desired)
+        transform.RotateAround(player.transform.position, new Vector3(0.0f, 1f, 0.0f), rotationSpeed * Time.deltaTime * mouseH / Screen.width);
+
+        //Re-focus camera onto player
+        transform.LookAt(player.transform.position);
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,56 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    public float health;
+    public float health = 100f;
 
-    public float attack;
-    public float defense;
-    public float thrust;
+    public float attack = 10f;
+    public float defense = 10f;
     public float combo_end_time = 0.5f;
     private float last_attack;
     private int combo;
 
-    public float gravity = 4f;
+    public float gravity = 3f;
     public float speed = 3.0f;
     public float turn = 0.5f;
-    public float jump = 20f;
+    public float jump = 30f;
 
     public float move_ver = 0f;
 
     private CharacterController character;
     private Animator anim;
+    AnimationEvent attackEvent;
+    GameObject attackBox;
+    public AnimationClip[] attackAnim;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         combo = 0;
         anim = GetComponent<Animator>();
         character = GetComponent<CharacterController>();
 
+        attackEvent = new AnimationEvent();
+        //Time into the attack animation where the damage (putting up the collider/trigger) will take place.  The timing can be experimented with.
+        attackEvent.time = 0.1F;
+        attackEvent.functionName = "PlayerMeleeTest";
+
+        for (int i = 0; i < attackAnim.Length; i++)
+        {
+            if (attackAnim[i].length > 0.4f)
+                attackEvent.time = 0.3f;
+            else
+                attackEvent.time = 0.1f;
+            attackAnim[i].AddEvent(attackEvent);
+        }
+
+        attackBox = transform.FindChild("PlayerMeleeTest").gameObject;
+        attackBox.SetActive(false);
         //StartCoroutine("Melee");
     }
-	
-	// Update is called once per frame
-	void Update () {
-	}
-    
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
 
     void FixedUpdate()
     {
-        playerMovement();
         playerAttack();
+        if (!anim.GetBool("IsAttacking"))           //Player should stop moving when attacking
+            playerMovement();
     }
 
     void playerMovement()
     {
+        //Controls are relative to camera
         float move_fwd = Input.GetAxis("Vertical");
         float move_hor = Input.GetAxis("Horizontal");
 
-        //transform.Rotate(0, move_hor * turn * Time.deltaTime, 0);
-        Vector3 movement = new Vector3 (move_hor * speed, 0, move_fwd * speed);
+        //Convert from camera-centered coordinates to fixed space-coordinates
+        float fixed_x = Camera.main.transform.forward.x * move_fwd + Camera.main.transform.right.x * move_hor;
+        float fixed_z = Camera.main.transform.forward.z * move_fwd + Camera.main.transform.right.z * move_hor;
+
+        Vector3 movement = new Vector3(fixed_x * speed, 0, fixed_z * speed);
+        
+        //Triggers different animations depending on speed (except vertical)
         anim.SetFloat("Speed", movement.magnitude);
+
+        //Rotates the player towards the direction of user-controlled motion (relative to the camera)
         if (movement.magnitude > 0)
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), turn);
 
@@ -69,15 +98,18 @@ public class PlayerController : MonoBehaviour {
         movement.y = move_ver;
         character.Move(movement * Time.deltaTime);
     }
+    IEnumerator PlayerMeleeTest()
+    {
+        attackBox.SetActive(true);
+        yield return new WaitForSeconds(0.5F);
+        attackBox.SetActive(false);
+    }
     void playerAttack()
     {
         if (combo == 0)
             if (Input.GetButtonDown("Fire1"))
             {
-                anim.SetBool("IsAttacking", true);
-                combo++;
-                print(combo);
-                last_attack = Time.time;
+                singleAttack();
             }
             else
                 anim.SetBool("IsAttacking", false);
@@ -85,13 +117,52 @@ public class PlayerController : MonoBehaviour {
         {
             if (Input.GetButtonDown("Fire1"))
             {
-                anim.SetBool("IsAttacking", true);
-                combo++;
-                print(combo);
-                last_attack = Time.time;
+                singleAttack();
             }
         }
         else
             combo = 0;
+    }
+    void singleAttack()
+    {
+        
+        anim.SetBool("IsAttacking", true);
+        combo++;
+        //print(combo);
+        last_attack = Time.time;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("EnemyMelee"))
+        {
+            int minusHealth = other.GetComponent<Damage>().getDamage();
+            if (minusHealth > health)
+            {
+                health = 0;
+                //Place for death animation and Destroy()
+            }
+            else
+            {
+                health -= minusHealth;
+                //Place for "taking damage" animation
+            }
+            Debug.Log("Player: " + health);
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag.Equals("EnemyRanged"))
+        {
+            int minusHealth = other.GetComponent<Damage>().getDamage();
+            if (minusHealth > health)
+            {
+                health = 0;
+            }
+            else
+            {
+                health -= minusHealth;
+            }
+            Debug.Log("Player: " + health);
+            Destroy(other.gameObject);
+        }
     }
 }
